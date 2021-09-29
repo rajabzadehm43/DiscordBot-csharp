4aabd4ae7027d53f3498002f0fd499c0e3aa5637
Mohammad Rajabzadeh <rajabzadehm43@gmail.com>
2021-09-29 12:44:49 +0330

    Add project files.

---
+++ DiscordBot-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord = DiscordBot_1.DiscordBot.Discord;
+
+namespace DiscordBot_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var token = "ODkxNjg3NDU5OTA0MTc2MTk5.YVB-qQ.KIdYQ0K5mFyYQ-xV9N5T1BtNemQ";
+            var discord = new DiscordBot_1.DiscordBot.Discord(token);
+
+            discord.RunBot().GetAwaiter().GetResult();
+
+            Task.Delay(-1).GetAwaiter().GetResult();
+        }
+    }
+}
