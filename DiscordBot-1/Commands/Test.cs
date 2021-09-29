3fe14d1b48ff0a92f7bbfcd6dc5932b6d055bd1f
Mohammad Rajabzadeh <rajabzadehm43@gmail.com>
2021-09-29 16:00:01 +0330

    Add Command System

---
+++ DiscordBot-1/Commands/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Sockets;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordBot_1.Commands
+{
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+        [Command("hello")]
+        public async Task SayHello()
+        {
+            await ReplyAsync($"Hello {Context.User.Mention}");
+        }
+    }
+}
--- DiscordBot-1/Program.cs
+++ DiscordBot-1/Program.cs
@@ -1,15 +1,97 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
-using Discord = DiscordBot_1.DiscordBot.Discord;
 
 namespace DiscordBot_1
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !");
+
+            var program = new Program();
+
+            program.StartBot().GetAwaiter().GetResult();
+            Task.Delay(-1).GetAwaiter().GetResult();
         }
+
+        #region Bot Global Variables
+
+        private const string Token = "ODkxNjg3NDU5OTA0MTc2MTk5.YVB-qQ.kmtcXwenD56_hRGQkxzHuVTEHQE";
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commandService;
+
+        #endregion
+
+        #region Service Manager
+
+        private readonly IServiceProvider _serviceProvider;
+
+        #endregion
+
+        public Program()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            _client = new DiscordSocketClient();
+            _commandService = new CommandService();
+
+            serviceCollection.AddSingleton(_client);
+            serviceCollection.AddSingleton(_commandService);
+
+            _client.Log += LogAsync;
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        #region Base Configuration
+
+        public async Task StartBot()
+        {
+            await _client.LoginAsync(TokenType.Bot, Token);
+            await _client.StartAsync();
+
+            await RegisterCommands();
+        }
+
+        private async Task LogAsync(LogMessage message)
+        {
+            Console.WriteLine(message);
+        }
+
+        #endregion
+
+        #region Command Handlers
+
+        private async Task RegisterCommands()
+        {
+            _client.MessageReceived += HandleMessage;
+            await _commandService.AddModulesAsync(Assembly.GetEntryAssembly(), _serviceProvider);
+        }
+
+        private async Task HandleMessage(SocketMessage arg)
+        {
+            var message = (SocketUserMessage) arg;
+            var context = new SocketCommandContext(_client, message);
+
+            int argPos = 0;
+            if (!message.HasCharPrefix('/', ref argPos))
+                return;
+
+            var result = await _commandService.ExecuteAsync(context, argPos, _serviceProvider);
+
+            if (!result.IsSuccess)
+                Console.WriteLine(result.ErrorReason);
+        }
+
+        #endregion
+
     }
 }
